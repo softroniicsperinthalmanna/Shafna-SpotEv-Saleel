7b017cb2423a13090cf2c78cf414a7234c5ff3cd
MATEBOOK <shafna.prmna@gmail.com>
2023-06-22 13:45:00 +0530

    secondPHPcommit

--- stationRegistration.php
+++ stationRegistration.php
@@ -5,6 +5,7 @@ $pass=$_POST['password'];
 $type=$_POST['user_type'];
 $name=$_POST['name'];
 $mob=$_POST['mobile_no'];
+// $loc=$_POST['location'];
 $sql1=mysqli_query($con,"INSERT INTO login_tb(email,password,user_type)values('$email','$pass','$type')");
 $userid=mysqli_insert_id($con);
 $sql2=mysqli_query($con,"INSERT INTO station_tb(name,mobile_no,login_id) VALUES ('$name','$mob','$userid')");
